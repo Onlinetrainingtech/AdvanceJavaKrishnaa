61c58c213e1a3e0a9c41b48a5b185d8e3155b623
azartechnet <azartechnet@gmail.com>
2024-08-21 16:53:05 +0530

    updated

---
+++ SpringAutowiringbyName/src/com/xyz/MainApp.java
@@ -0,0 +1,15 @@
+package com.xyz;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext cnt=new ClassPathXmlApplicationContext();
+		TextEditor txt=cnt.
+
+	}
+
+}
